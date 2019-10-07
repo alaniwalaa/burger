16b9a63ff3e2aad91dd71d905b54ed6309fc4f7e
walaa <w.alani2010@yahoo.com>
2019-10-07 09:52:29 -0700

    app finalized

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('burger 1');
+INSERT INTO burgers (name) VALUES ('burger 2');
+INSERT INTO burgers (name, devoured) VALUES ('burger 3', true);
+INSERT INTO burgers (name, devoured) VALUES ('burger 4', true);
+INSERT INTO burgers (name, devoured) VALUES ('burger 5', true);
+INSERT INTO burgers (name) VALUES ('Jumbo');
